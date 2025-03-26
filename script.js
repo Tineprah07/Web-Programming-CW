876200fbe2b540c1a7dbd3b909a74ee8bd8f0c32
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-03-26 22:45:10 +0000

    updates

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.querySelector("#menu-icon");
+    const menuBox = document.querySelector("#menu-box");
+    const body = document.querySelector("body");
 
+    menuBtn.addEventListener("click", function (event) {
+        event.stopPropagation();
+        menuBox.classList.toggle("active");
+        menuBtn.classList.toggle("pushed");
+        body.classList.toggle("overlay-active");
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!menuBox.contains(event.target) && !menuBtn.contains(event.target)) {
+            menuBox.classList.remove("active");
+            menuBtn.classList.remove("pushed");
+            body.classList.remove("overlay-active");
+        }
+    });
+});
