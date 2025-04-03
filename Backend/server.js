8e3913c2cf0c318681376f66e392d6ae1ddefceb
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-04-03 16:17:38 +0100

    updating server

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+// Import the Express module
+import express from 'express';
+
+const app = express();
+const PORT = 8080;
+app.use(express.static('../Frontend'));
+
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
