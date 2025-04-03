a3e1d0368625b343461be86a5be461949c30530c
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-04-04 00:36:57 +0100

    updating files

--- results.sql
+++ results.sql
@@ -5,11 +5,12 @@ CREATE TABLE results (
             position INTEGER NOT NULL,
             time TEXT NOT NULL
         );
-INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        26\n    ','\n',char(10)));
-INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        02:\n        44\n    ','\n',char(10)));
-INSERT INTO results VALUES(3,3,replace('\n        00:\n        00:\n        03:\n        58\n    ','\n',char(10)));
-INSERT INTO results VALUES(4,4,replace('\n        00:\n        00:\n        04:\n        44\n    ','\n',char(10)));
-INSERT INTO results VALUES(5,5,replace('\n        00:\n        00:\n        04:\n        97\n    ','\n',char(10)));
+INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        52\n    ','\n',char(10)));
+INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        01:\n        85\n    ','\n',char(10)));
+INSERT INTO results VALUES(3,3,replace('\n        00:\n        00:\n        03:\n        40\n    ','\n',char(10)));
+INSERT INTO results VALUES(4,4,replace('\n        00:\n        00:\n        03:\n        67\n    ','\n',char(10)));
+INSERT INTO results VALUES(5,5,replace('\n        00:\n        00:\n        05:\n        52\n    ','\n',char(10)));
+INSERT INTO results VALUES(6,6,replace('\n        00:\n        00:\n        13:\n        32\n    ','\n',char(10)));
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('results',5);
+INSERT INTO sqlite_sequence VALUES('results',6);
 COMMIT;
