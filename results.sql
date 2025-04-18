3e1d7cf78365f68323f0936fcec420b963b9e225
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-04-18 18:00:28 +0100

    updates

--- results.sql
+++ results.sql
@@ -5,10 +5,13 @@ CREATE TABLE results (
             position INTEGER NOT NULL,
             time TEXT NOT NULL
         );
-INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        03:\n        36\n    ','\n',char(10)));
-INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        04:\n        11\n    ','\n',char(10)));
-INSERT INTO results VALUES(3,3,replace('\n        00:\n        00:\n        04:\n        43\n    ','\n',char(10)));
+INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        02:\n        59\n    ','\n',char(10)));
+INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        03:\n        44\n    ','\n',char(10)));
+INSERT INTO results VALUES(3,3,replace('\n        00:\n        00:\n        04:\n        09\n    ','\n',char(10)));
 INSERT INTO results VALUES(4,4,replace('\n        00:\n        00:\n        04:\n        73\n    ','\n',char(10)));
+INSERT INTO results VALUES(5,5,replace('\n        00:\n        00:\n        05:\n        01\n    ','\n',char(10)));
+INSERT INTO results VALUES(6,6,replace('\n        00:\n        00:\n        05:\n        29\n    ','\n',char(10)));
+INSERT INTO results VALUES(7,7,replace('\n        00:\n        00:\n        05:\n        54\n    ','\n',char(10)));
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('results',4);
+INSERT INTO sqlite_sequence VALUES('results',7);
 COMMIT;
