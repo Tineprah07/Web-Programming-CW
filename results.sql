b1607f7bd8cd0383b8bcba466993328385d45fe3
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-05-10 14:13:55 +0100

    updates

--- results.sql
+++ results.sql
@@ -5,8 +5,10 @@ CREATE TABLE results (
             position INTEGER NOT NULL,
             time TEXT NOT NULL
         );
-INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        02:\n        66\n    ','\n',char(10)));
-INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        03:\n        99\n    ','\n',char(10)));
+INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        79\n    ','\n',char(10)));
+INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        02:\n        71\n    ','\n',char(10)));
+INSERT INTO results VALUES(3,3,replace('\n        00:\n        00:\n        03:\n        34\n    ','\n',char(10)));
+INSERT INTO results VALUES(4,4,replace('\n        00:\n        00:\n        04:\n        16\n    ','\n',char(10)));
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('results',2);
+INSERT INTO sqlite_sequence VALUES('results',4);
 COMMIT;
