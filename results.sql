33f706cbaa7086b5818952084826f8af2e37ef35
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-05-12 00:26:37 +0100

    update

--- results.sql
+++ results.sql
@@ -1,3 +1,4 @@
+-- Including this file for testing purpose
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE results (
@@ -5,10 +6,8 @@ CREATE TABLE results (
             position INTEGER NOT NULL,
             time TEXT NOT NULL
         );
-INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        79\n    ','\n',char(10)));
-INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        02:\n        71\n    ','\n',char(10)));
-INSERT INTO results VALUES(3,3,replace('\n        00:\n        00:\n        03:\n        34\n    ','\n',char(10)));
-INSERT INTO results VALUES(4,4,replace('\n        00:\n        00:\n        04:\n        16\n    ','\n',char(10)));
+INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        16\n    ','\n',char(10)));
+INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        01:\n        91\n    ','\n',char(10)));
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('results',4);
+INSERT INTO sqlite_sequence VALUES('results',2);
 COMMIT;
