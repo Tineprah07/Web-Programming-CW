511828fc2e07deb2a484c03be1f71ba1c6fe7ed6
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-05-12 23:41:55 +0100

    updates

--- results.sql
+++ results.sql
@@ -6,10 +6,10 @@ CREATE TABLE results (
             time TEXT NOT NULL,
             runnerId TEXT
         );
-INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        05:\n        14\n    ','\n',char(10)),'a1');
-INSERT INTO results VALUES(2,2,replace('\n        00:\n        01:\n        21:\n        93\n    ','\n',char(10)),'A2');
-INSERT INTO results VALUES(3,3,replace('\n        00:\n        01:\n        29:\n        61\n    ','\n',char(10)),'A6');
-INSERT INTO results VALUES(4,4,replace('\n        00:\n        01:\n        35:\n        03\n    ','\n',char(10)),'A7');
+INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        21\n    ','\n',char(10)),'A1');
+INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        06:\n        94\n    ','\n',char(10)),'A5');
+INSERT INTO results VALUES(3,3,replace('\n        00:\n        00:\n        11:\n        46\n    ','\n',char(10)),'A8');
+INSERT INTO results VALUES(4,4,replace('\n        00:\n        00:\n        19:\n        36\n    ','\n',char(10)),'A2');
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('results',4);
 COMMIT;
