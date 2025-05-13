920029930d2f84106949de1742b3f0be74649f21
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-05-14 00:27:14 +0100

    updates

--- results.sql
+++ results.sql
@@ -6,8 +6,8 @@ CREATE TABLE results (
             time TEXT NOT NULL,
             runnerId TEXT
         );
-INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        41\n    ','\n',char(10)),'B1');
-INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        07:\n        44\n    ','\n',char(10)),'b5');
+INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        90\n    ','\n',char(10)),'A2');
+INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        05:\n        86\n    ','\n',char(10)),'A9');
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('results',2);
 COMMIT;
