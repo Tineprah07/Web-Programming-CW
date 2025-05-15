dd43963013e4f71051203009b04ee09578ef7298
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-05-15 15:41:46 +0100

    updates

--- Frontend/script.js
+++ Frontend/script.js
@@ -284,7 +284,7 @@ submitBtn.addEventListener("click", async () => {
         });
 
         if (response.ok) {
-            alert("✅ Results submitted successfully!. Go to the results page to see them.");
+            alert("✅ Results submitted successfully!. Go to the results page(Menu) to see them.");
             localStorage.removeItem("raceRecords");
             location.reload(); // Clear the UI
         } else {
--- results.sql
+++ results.sql
@@ -6,8 +6,7 @@ CREATE TABLE results (
             time TEXT NOT NULL,
             runnerId TEXT
         );
-INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        01:\n        11\n    ','\n',char(10)),'a1');
-INSERT INTO results VALUES(2,2,replace('\n        00:\n        00:\n        03:\n        78\n    ','\n',char(10)),'b1');
+INSERT INTO results VALUES(1,1,replace('\n        00:\n        00:\n        03:\n        64\n    ','\n',char(10)),'a1');
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('results',2);
+INSERT INTO sqlite_sequence VALUES('results',1);
 COMMIT;
